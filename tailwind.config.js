468436cce19bd4053cd214c575a33eadc343983a
Ital023 <contadoitalo911@gmail.com>
2024-08-25 12:47:56 -0300

    feat: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#55BC04',
+        secondary: '#C2C2C2',
+        tertiary: '#FFFFFF',
+      }
+    },
   },
   plugins: [],
 }
