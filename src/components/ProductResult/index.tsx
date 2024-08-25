fb3149de0bf8c53cdeeaf55048170fd594497e96
Ital023 <contadoitalo911@gmail.com>
2024-08-25 14:50:44 -0300

    feat: ProductResult+

---
+++ src/components/ProductResult/index.tsx
@@ -0,0 +1,8 @@
+export default function ProductResult() {
+  return (
+    <div className="border border-primary p-2 rounded-md">
+      <h3 className="text-quartenary text-xs">PC Gamer Pro</h3>
+      <p className="text-primary text-base font-bold">R$ 1200.00</p>
+    </div>
+  );
+}
--- src/components/ProductsResultContainer/index.tsx
+++ src/components/ProductsResultContainer/index.tsx
@@ -1,10 +1,14 @@
+import ProductResult from "../ProductResult";
+
 export default function ProductsResultContainer() {
   return (
-    <div className="bg-tertiary rounded-md p-5 flex flex-col gap-3">
-      <div className="border border-primary p-2 rounded-md">
-        <h3 className="text-quartenary text-xs">PC Gamer Pro</h3>
-        <p className="text-primary text-base font-bold">R$ 1200.00</p>
-      </div>
+    <div className="bg-tertiary rounded-md p-5 flex flex-col gap-3 md:max-w-[960px] md:mx-auto">
+        <ProductResult />
+        <ProductResult />
+        <ProductResult />
+        <ProductResult />
+        <ProductResult />
+        <ProductResult />
     </div>
   );
 }
